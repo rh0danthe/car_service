5fed19583d3c05a614c3b03c1b2c2e9d5afa8753
rh0danthe <rita.pichushkina.04@mail.ru>
2023-11-01 01:22:38 +0300

    победа

--- car_service/Scripts/001_initial.sql
+++ car_service/Scripts/001_initial.sql
@@ -1,24 +1,24 @@
-﻿CREATE TABLE IF NOT EXISTS Cars (
-     Id serial NOT NULL PRIMARY KEY, 
-     Brand text,
-     Model text,
-     YearOfRelease smallint,
-     VINcode text
+﻿CREATE TABLE IF NOT EXISTS "Cars" (
+     "Id" serial NOT NULL PRIMARY KEY, 
+     "Brand" text,
+     "Model" text,
+     "YearOfRelease" smallint,
+     "VINcode" text
 );
 
-CREATE TABLE IF NOT EXISTS Clients (
-    Id serial NOT NULL PRIMARY KEY,
-    Name text,
-    Surname text,
-    Adress text,
-    PhoneNumber text
+CREATE TABLE IF NOT EXISTS "Clients" (
+    "Id" serial NOT NULL PRIMARY KEY,
+    "Name" text,
+    "Surname" text,
+    "Adress" text,
+    "PhoneNumber" text
 );
 
-CREATE TABLE IF NOT EXISTS Orders (
-    Id serial NOT NULL PRIMARY KEY,
-    CarId integer REFERENCES Cars(Id) ON DELETE CASCADE,
-    ClientId integer REFERENCES Clients(Id) ON DELETE CASCADE,
-    DateTime timestamp,
-    WorkDescription text,
-    Status smallint
+CREATE TABLE IF NOT EXISTS "Orders" (
+    "Id" serial NOT NULL PRIMARY KEY,
+    "CarId" integer REFERENCES "Cars"("Id") ON DELETE CASCADE,
+    "ClientId" integer REFERENCES "Clients"("Id") ON DELETE CASCADE,
+    "DateTime" timestamp,
+    "WorkDescription" text,
+    "Status" smallint
 );
